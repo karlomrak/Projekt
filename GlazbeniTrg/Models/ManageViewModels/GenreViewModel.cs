e49128143867c1eed42669a1869a0c7ed7e5f83a
Karlo Mrakovčić <karlo.mrakovcic@fer.hr>
2018-02-19 02:04:40 +0100

    dovrsen web shop glazbenih djela

---
+++ GlazbeniTrg/Models/ManageViewModels/GenreViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlazbeniTrg.Models.ManageViewModels
+{
+    public class GenreViewModel
+    {
+        public List<Genre> PrefGenres { get; set; }
+
+        public List<Genre> Genres { get; set; }
+        
+        public bool[] Values { get; set; }
+    }
+}
